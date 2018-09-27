96a7adeff73b1cc10597d5f8bc720044488cbe99
Hunter Rancourt <hunter.rancourt@gmail.com>
2018-09-26 21:07:27 -0500

    ejected config and setup css modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
       </div>
     );
   }
