458ac1492c81700efbde30a295f1b2c41edadc4f
Hunter Rancourt <hunter.rancourt@gmail.com>
2018-09-27 18:11:23 -0500

    setup routing for Auth component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import classes from './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import DevTools from 'mobx-react-devtools';
+import classes from './App.css';
+import Layout from './hoc/Layout/Layout';
+import Auth from './components/Auth/Auth';
+
+//@TODO should logic for routing if the user is auth'd be here
+//@TODO or should the Auth component redirect?
+// if handled by Auth component then the state here doesn't matter
+//* could default to /login and let it redirect if user is auth'd
 
 class App extends Component {
 	state = {
-
-	}
+		userAuthenticated: false,
+	};
 
 	render() {
 		return (
 			<div className={classes.App}>
+				<Layout>
+					<Switch>
+						<Route path="/login" component={Auth} />
+						<Redirect path="/" exact to="/login" />
+					</Switch>
+				</Layout>
 
 				<DevTools />
 			</div>
