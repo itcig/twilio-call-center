7f353e6d06c446c67f5b81f7ff04fd9752b728ee
Hunter Rancourt <hunter.rancourt@gmail.com>
2018-10-17 14:00:36 -0500

    added Material Design for Bootstrap 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
